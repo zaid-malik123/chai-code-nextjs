c4c408321eb5ad4675bc365d93f993621fd15ca2
Zaid Malik <mzaidmalik25@gmail.com>
2026-03-29 13:01:08 +0530

    feat: replace Resend with Nodemailer for email service

--- src/helpers/sendVerificationMail.ts
+++ src/helpers/sendVerificationMail.ts
@@ -1,23 +1,36 @@
-import { resend } from "@/lib/resend";
 import { ApiResponse } from "@/types/ApiResponse";
 import VerificationEmail from "../../emails/VerificationEmail";
+import { transporter } from "@/lib/nodemailer";
+import { render } from "@react-email/render";
 
 export const sendVerificationMail = async (
   email: string,
   verifyCode: string,
-  userName: string,
+  userName: string
 ): Promise<ApiResponse> => {
   try {
-    await resend.emails.send({
-      from: "onboarding@resend.dev",
+    const html = await render(
+      VerificationEmail({ userName, otp: verifyCode })
+    );
+
+     await transporter.sendMail({
+      from: `"Mystery App" <${process.env.EMAIL_USER}>`,
       to: email,
       subject: "Mystery Message Verification Code",
-      react: VerificationEmail({ userName, otp: verifyCode }),
+      html: html,
     });
-    console.log("send mail")
-    return { success: true, message: 'Verification email sent successfully.' };
+
+
+    return {
+      success: true,
+      message: "Verification email sent successfully.",
+    };
   } catch (error) {
     console.error("Error sending verification email:", error);
-    return { success: false, message: "Failed to send verification email." };
+
+    return {
+      success: false,
+      message: "Failed to send verification email.",
+    };
   }
 };
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,27 +1,29 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
-export { default } from 'next-auth/middleware';
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+export { default } from "next-auth/middleware";
 
-export const config = {
-  matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*'],
+const config = {
+  matcher: ["/dashboard/:path*", "/sign-in", "/sign-up", "/", "/verify/:path*"],
 };
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
-
+  if (url.pathname.startsWith("/api/auth")) {
+    return NextResponse.next();
+  }
   if (
     token &&
-    (url.pathname.startsWith('/sign-in') ||
-      url.pathname.startsWith('/sign-up') ||
-      url.pathname.startsWith('/verify') ||
-      url.pathname === '/')
+    (url.pathname.startsWith("/sign-in") ||
+      url.pathname.startsWith("/sign-up") ||
+      url.pathname.startsWith("/verify") ||
+      url.pathname === "/")
   ) {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
-  if (!token && url.pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/sign-in', request.url));
+  if (!token && url.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
   return NextResponse.next();
